10d4675346fdd48373f8a42bce68372a37cca2df
Rasmus Nielsen <rasmus@easify.dk>
2019-09-24 14:06:30 +0200

    Add subscriptions endpoints for customer

--- localstripe/resources.py
+++ localstripe/resources.py
@@ -736,6 +736,39 @@ class Customer(StripeObject):
         obj = cls._api_retrieve(id)
         return obj.tax_ids
 
+    @classmethod
+    def _api_add_subscription(cls, id, **data):
+        obj = Subscription(customer=id, **data)
+        return obj
+
+    @classmethod
+    def _api_retrieve_subscription(cls, id, subscription_id, **kwargs):
+        if kwargs:
+            raise UserError(400, 'Unexpected ' + ', '.join(kwargs.keys()))
+
+        obj = Subscription._api_retrieve(subscription_id)
+
+        if obj.customer != id:
+            raise UserError(400, 'Customer ' + id + ' does not have a subscription with ID ' + subscription_id)
+
+        return obj
+
+    @classmethod
+    def _api_update_subscription(cls, id, subscription_id, **data):
+        obj = Subscription._api_retrieve(subscription_id)
+
+        if obj.customer != id:
+            raise UserError(400, 'Customer ' + id + ' does not have a subscription with ID ' + subscription_id)
+
+        return obj._update(**data)
+
+    @classmethod
+    def _api_list_subscriptions(cls, id, **kwargs):
+        if kwargs:
+            raise UserError(400, 'Unexpected ' + ', '.join(kwargs.keys()))
+
+        return cls._api_retrieve(id).subscriptions
+
 
 extra_apis.extend((
     ('POST', '/v1/customers/{id}/sources', Customer._api_add_source),
@@ -748,6 +781,14 @@ extra_apis.extend((
     # Delete single source by id:
     ('DELETE', '/v1/customers/{id}/sources/{source_id}',
      Customer._api_remove_source),
+
+
+    # Subscription endpoints
+    ('GET', '/v1/customers/{id}/subscriptions', Customer._api_list_subscriptions),
+    ('POST', '/v1/customers/{id}/subscriptions', Customer._api_add_subscription),
+    ('GET', '/v1/customers/{id}/subscriptions/{subscription_id}', Customer._api_retrieve_subscription),
+    ('POST', '/v1/customers/{id}/subscriptions/{subscription_id}', Customer._api_update_subscription),
+
     # This is the old API route:
     ('POST', '/v1/customers/{id}/cards', Customer._api_add_source),
     ('POST', '/v1/customers/{id}/tax_ids', Customer._api_add_tax_id),
