4f674b4549d91d346bd728ed4592bb6f58f3f9f2
Tyler Gregory <tdgregory@protonmail.com>
2021-09-28 13:56:24 -0500

    MOre debug

--- localstripe/webhooks.py
+++ localstripe/webhooks.py
@@ -73,7 +73,7 @@ async def _send_webhook(event):
                         logger.info('webhook "%s" successfully delivered'
                                     % event.type)
                     else:
-                        logger.warning(f'webhook "{event.type}" failed with response code {r.status} and body:\n{r.text()}')
+                        logger.warning(f'webhook "{event.type}" failed with response code {r.status} and body:\n{await r.text()}')
             except aiohttp.client_exceptions.ClientError as e:
                 logger.warning('webhook "%s" failed: %s' % (event.type, e))
 
