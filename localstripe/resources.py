bab5fc046b73a62263b097c467df6010c1d80cf9
Tyler Gregory <tdgregory@protonmail.com>
2021-09-29 21:29:32 -0500

    Correct retrieve call

--- localstripe/resources.py
+++ localstripe/resources.py
@@ -2058,7 +2058,7 @@ class PaymentIntent(StripeObject):
 
         if confirm:
             cls._api_confirm(obj.id)
-            obj = cls._api_retrieve(id)
+            obj = cls._api_retrieve(obj.id)
 
         return obj
 
